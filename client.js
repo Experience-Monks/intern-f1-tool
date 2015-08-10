6f387d39336976b244279e04a1aed1f66313e89d
Mikko Haapoja <me@mikkoh.com>
2015-08-10 16:59:28 -0400

    Implemented an entry where you can enter the new state name

--- client.js
+++ client.js
@@ -2,6 +2,5 @@ var framework = require('./lib/framework');
 
 document.body.style.margin = 0;
 document.body.style.width = document.body.style.height = '100%';
-document.body.style.background = '#F00';
 
 framework.init();
